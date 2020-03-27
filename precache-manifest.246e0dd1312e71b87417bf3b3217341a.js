dd94fe0617f25ea50f1ec6400d0ef8d72f27dc7c
Gianluca Maccioni <gianluca.maccioni.uk@gmail.com>
2020-03-27 14:20:38 +0000

    Updates

---
+++ precache-manifest.246e0dd1312e71b87417bf3b3217341a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19f1af0ec1f9c012e0271075f1f39d7d",
+    "url": "/react-redux-test/index.html"
+  },
+  {
+    "revision": "35022c43b91809de3c48",
+    "url": "/react-redux-test/static/css/main.f50b18cd.chunk.css"
+  },
+  {
+    "revision": "41692e11235170a670d7",
+    "url": "/react-redux-test/static/js/2.6c1138f9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-redux-test/static/js/2.6c1138f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35022c43b91809de3c48",
+    "url": "/react-redux-test/static/js/main.f567dc74.chunk.js"
+  },
+  {
+    "revision": "cc51534eed371f33de98",
+    "url": "/react-redux-test/static/js/runtime-main.80e2d0e1.js"
+  },
+  {
+    "revision": "8dad80289a1ece380591ad143b616747",
+    "url": "/react-redux-test/static/media/logo.8dad8028.svg"
+  }
+]);
